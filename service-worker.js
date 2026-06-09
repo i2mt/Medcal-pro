447d75cdc4e9044a2649ae8a63031e9e3dfa863b
i2mt <mohammad.mahdi.ta@gmail.com>
2026-06-09 04:13:48 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Always tries to fetch the latest version.
 // Falls back to cache only when offline.
 
-const CACHE_NAME = 'FoxiMed_v3.5.3';
+const CACHE_NAME = 'FoxiMed_v3.5.5';
 
 const urlsToCache = [
     './',
